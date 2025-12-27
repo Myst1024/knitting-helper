afcb07c6f78f14408fc5ae581be5ddc776c0fa1c
Gunnar Carlson <gunnar.carlson@gmail.com>
2025-12-27 11:55:48 -0600

    Initial Commit

---
+++ knitting helper/knitting_helperApp.swift
@@ -0,0 +1,17 @@
+//
+//  knitting_helperApp.swift
+//  knitting helper
+//
+//  Created by Gunnar Carlson on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct knitting_helperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
